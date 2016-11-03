3f046a114d05f16c5b975d4f99fe15d57eec8fe1
gouti318 <gouti318@gmail.com>
2016-11-04 00:29:29 +0530

    Working model till infoWindow

---
+++ js/main.js
@@ -0,0 +1,108 @@
+var map;
+var model_data = ko.observableArray([
+  {
+    name: "Jim Corbett National Park",
+    lat: 29.3255,
+    lng: 78.567,
+    location: "Nainital, Uttarakhand, India"
+  },
+  {
+    name: "Port Blair",
+    lat: 11.4006,
+    lng: 92.4416,
+    location: "South Andaman, Andaman and Nicobar Islands, India"
+  },
+  {
+    name: "Kodaikanal",
+    lat: 10.28,
+    lng: 77.48,
+    location: "Dindigul, TamilNadu, India"
+  },
+  {
+    name: "Darjeeling",
+    lat: 27.080,
+    lng: 88.26,
+    location: "Darjeeling, West Bengal, India"
+  },
+  {
+    name: "Gangtok",
+    lat: 27.33,
+    lng: 88.62,
+    location: "East Sikkim, Sikkim, India"
+  },
+  {
+    name: "Agatti",
+    lat: 10.50,
+    lng: 73,
+    location: "Lakshadweep, India"
+  },
+  {
+    name: "Goa",
+    lat: 15.498605,
+    lng: 73.829262,
+    location: "Goa, India"
+  },
+  {
+    name: "Manali",
+    lat:32.27,
+    lng: 77.17,
+    location: "Kullu, Himachal Pradesh, India"
+  },
+  {
+    name: "Hampi",
+    lat: 15.335,
+    lng: 76.462,
+    location: "Bellary, Karnataka, India"
+  },
+  {
+    name: "Coorg",
+    lat: 12.4208,
+    lng: 75.7397,
+    location: "Kodagu, Karnataka, India"
+  }
+]);
+
+var markers = [];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 21.767, lng: 78.8718},
+    zoom: 5
+  });
+
+  var l_infowindow = new google.maps.InfoWindow();
+
+  for(var i = 0; i < model_data().length; i++) {
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(model_data()[i].lat, model_data()[i].lng),
+      map: map,
+      title: model_data()[i].name,
+      animation: google.maps.Animation.DROP,
+      id: i,
+      location: model_data()[i].location
+    });
+
+    markers.push(marker);
+
+    marker.addListener('click', function() {
+      generateInfoWindow(this, l_infowindow);
+    });
+
+  }
+}
+
+function generateInfoWindow(marker, infowindow) {
+  console.log(marker.location);
+  if(infowindow.marker != marker) {
+    infowindow.marker = marker;
+    var content = '<div><h1>' + marker.title + '</h1><hr>' +
+                  '<br><p>' + marker.location + '</p></div>';
+    infowindow.setContent(content);
+    infowindow.open(map,marker);
+
+    infowindow.addListener('closeclick',function(){
+      infowindow.setMarker(null);
+    });
+  }
+}
